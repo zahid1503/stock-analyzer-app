0d233c1fa605bb91831b7d612591731b319b1c6c
Zahid <mohammed.zahid019@gmail.com>
2023-08-25 22:32:21 +0530

    initial commit

---
+++ mohammed-zahid019-ME_QMONEY_V2-master/mohammed-zahid019-ME_QMONEY_V2-master/qmoney/src/main/java/com/crio/warmup/stock/portfolio/PortfolioManagerFactory.java
@@ -0,0 +1,21 @@
+
+package com.crio.warmup.stock.portfolio;
+
+import org.springframework.web.client.RestTemplate;
+
+public class PortfolioManagerFactory {
+
+
+public static  PortfolioManager getPortfolioManager(RestTemplate restTemplate) {
+  return new PortfolioManagerImpl();
+
+    
+
+  }
+
+
+
+
+
+
+}
